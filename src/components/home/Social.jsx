af3659d61e6895d745dbc1c033b471206bc16142
Gedeon Kakona <gedeonkakona@gmail.com>
2022-12-06 11:37:54 +0200

    building home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://twitter.com/GedeonKakona"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+      <a
+        href="https://github.com/gedeon809"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/gedeon-kakona-69627a1a4/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
