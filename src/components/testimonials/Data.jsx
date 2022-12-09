9a9f29cd483cb127bc9c914fde9febdfa61c2f51
Gedeon Kakona <gedeonkakona@gmail.com>
2022-12-09 15:06:40 +0200

    add testimonials components

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import Image1 from '../../assets/ba1.jpg';
+import Image2 from '../../assets/ceo.jpeg';
+import Image3 from '../../assets/fidel.png';
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: 'Nissi Mwadi',
+    description:
+      '💻Always paying attention to client requirements. I loved the website he built for my business💖',
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: 'Clement Betu',
+    description:
+      'A really good job, all aspects of the project were followed step by step and with great success.',
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: 'David Malaba',
+    description:
+      'A really good developer and the application was so clear and easy to use',
+  },
+];
